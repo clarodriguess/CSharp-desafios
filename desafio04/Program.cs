32e9c14aa98ae2157f45ed399dfbeaf7432dea65
clarisse <clarissebleasby@gmail.com>
2026-06-16 11:52:08 -0300

    desafio04

--- desafio04/Program.cs
+++ desafio04/Program.cs
@@ -1,1 +1,23 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.Write("Valor da compra: R$ ");
+//double valor = Convert.ToDouble(Console.ReadLine());
+double valor = double.Parse(Console.ReadLine());
+
+double desconto = 0;
+
+if (valor < 100)
+{
+    desconto = 0;
+}
+else if (valor < 500)
+{
+    desconto = 0.05;
+}
+else
+{
+    desconto = 0.10;
+}
+
+double valorFinal = valor - (valor * desconto);
+
+Console.WriteLine($"Desconto aplicado: {desconto * 100}%");
+Console.WriteLine($"Valor a pagar: R$ {valorFinal:F2}"); //2casas decimais
