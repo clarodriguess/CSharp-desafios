bf9ac76d3bc7d7e604703bb3eff350c95b55564e
clarisse <clarissebleasby@gmail.com>
2026-06-16 11:32:34 -0300

    ✨ desafio 01

---
+++ desafio01/Program.cs
@@ -0,0 +1,11 @@
+﻿	
+	
+		Console.WriteLine("Digite em que ano estamos: (AAAA)");
+		int anoAtual = int.Parse(Console.ReadLine());
+		
+		Console.WriteLine("Digite o ano do seu nascimento: (AAAA)");
+		int anoNascimento = int.Parse(Console.ReadLine());
+		
+		int idade = anoAtual - anoNascimento;
+		Console.WriteLine($"Você tem {idade} anos");
+	
